5aebcad147f39eaddf20da34d9d7827d125fa04a
buhaymarella <marella.gutierrez84@gmail.com>
2022-04-12 21:35:46 +0800

    INDEX ADDED

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>MELOMANIAC</title>
+</head>
+<body>
+	<nav>
+		<div class="brand_name">
+			<p>ROCK TRACK CORNER</p>
+		</div>
+		<ul>
+			<li><a href="#">SONG 1</a></li>
+			<li><a href="#">SONG 2</a></li>
+			<li><a href="#">SONG 3</a></li>
+			<li><a href="#">SONG 4</a></li>
+			<li><a href="#">SONG 5</a></li>
+		</ul>
+	</nav>
+	<div class="image">
+		<div class="container">
+			<h1><span>WELCOME TO MELOMANIAC</span></h1>
+			<span class="caption"> Rock Out! Play Out! </span>
+		</div>		
+	</div>
+	<footer>
+		<div class="footer">
+			Copyright &copy; 2022 Marella Buhay
+		</div>
+	</footer>
+
+</body>
+</html>
