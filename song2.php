8faba827e12cb6a6dfdbef7b1763b5b90074e984
buhaymarella <marella.gutierrez84@gmail.com>
2022-04-13 01:56:59 +0800

    SONG 2 ADDED

---
+++ song2.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>MELOMANIAC</title>
+</head>
+<body>
+	<nav>
+		<div class="brand_name">
+			<p>ROCK TRACK CORNER</p>
+		</div>
+		<ul>
+			<li><a href="song1.php">SONG 1</a></li>
+			<li><a href="song2.php" class="active">SONG 2</a></li>
+			<li><a href="song3.php">SONG 3</a></li>
+			<li><a href="song4.php">SONG 4</a></li>
+			<li><a href="song5.php">SONG 5</a></li>
+		</ul>
+	</nav>
+	<div class="image">
+		<div class="container">
+			<h1><span>WELCOME TO MELOMANIAC</span></h1>
+			<span class="caption"> Rock Out! Play Out! </span>
+		</div>		
+	</div>
+	<a href="https://www.youtube.com/watch?v=tpAMJ_yzltU"><img src="album/paramore.jfif" class="album"></a>
+    <h6 class="click"> Click on the photo to listen and play along! </h6>
+ </body>
+    <div class="lyrics">
+
+    	<?php
+    		echo "<br/>";
+    		echo "<br/>";
+    		echo "<h2 class='text-primary'>ROSE COLORED BOY</h2>";
+    		echo "<p class='lead'> Paramore </p>";
+    		echo "<br/>";
+
+    		echo "<p>[INTRO: F]</p>";
+    		echo "<p>Dm &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp  &nbsp C &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp F</p>";
+    		echo "<p>Low-key, no pressure, just hang with me and my weather</p>";
+    		echo "<p>Dm &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp  &nbsp C &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp F</p>";
+    		echo "<p>Low-key, no pressure, just hang with me and my weather</p>";
+
+    		echo "<p>[VERSE 1]</p>";
+    		echo "<p>F &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>Rose-colored boy</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Dm</p>";
+    		echo "<p>I hear you making all that noise</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp C</p>";
+    		echo "<p>About the world you want to see</p>";
+    		echo "<p>&nbspF &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>And oh, I'm so annoyed</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Dm &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp C</p>";
+    		echo "<p>'Cause I just killed off what was left of the optimist in me</p><br/><br/>";
+
+    		echo "<p>[REFRAIN]</p>";
+    		echo "<p>Bb &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp C &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Dm</p>";
+    		echo "<p>Hearts are breaking, Wars are raging on</p>";
+    		echo "<p>&nbsp &nbspC</p>";
+    		echo "<p>And I have taken my glasses off</p>";
+    		echo "<p>Bb &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp C</p>";
+    		echo "<p>You got me nervous</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Dm</p>";
+    		echo "<p>I'm right at the end of my rope</p>";
+    		echo "<p></p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp C</p>";
+    		echo "<p>A half empty girl</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbspF</p>";
+    		echo "<p>Don't make me laugh, I'll choke</p><br/><br/>";
+
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  F &nbsp &nbsp&nbsp&nbsp Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbspBb</p>";
+    		echo "<p>Just let me cry a little bit longer</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  F &nbsp &nbsp&nbsp&nbsp Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbspBb C</p>";;
+    		echo "<p>I ain't gon' smile if I don't want to</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Bb &nbsp &nbsp&nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbspC</p>";
+    		echo "<p>Hey man, we all can't be like you</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Bb &nbsp &nbsp&nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbspC</p>";
+    		echo "<p>I wish we were all rose-colored too</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbspF</p>";
+    		echo "<p>My rose-colored boy</p>";
+    		echo "<p>Dm &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp  &nbsp C &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp F</p>";
+    		echo "<p>Low-key, no pressure, just hang with me and my weather</p><br/><br/>";
+    		
+
+    		echo "<p>[VERSE]</p>";
+    		echo "<p>F&nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbspC</p>";
+    		echo "<p>I want you to stop insisting that I'm not a lost cause</p>";
+    		echo "<p></p>";
+    		echo "<p>'Cause I've been through a lot</p>";
+    		echo "<p></p>";
+    		echo "<p>Really all I've got is just to stay pissed off</p>";
+    		echo "<p></p>";
+    		echo "<p>If it's all right by you</p>";
+    		echo "<p></p>";
+    		echo "<p>Hearts are breaking, wars are raging on</p>";
+    		echo "<p></p>";
+    		echo "<p>And I have taken my glasses off</p>";
+    		echo "<p></p>";
+    		echo "<p>You got me nervous</p>";
+    		echo "<p></p>";
+    		echo "<p>And you’re turning it into a joke</p>";
+    		echo "<p></p>";
+    		echo "<p>A half empty girl</p>";
+    		echo "<p></p>";
+    		echo "<p>Don't make me laugh, I'll...</p><br/><br/>";
+
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p></p>";
+    		echo "<p>Just let me cry a little bit longer</p>";
+    		echo "<p></p>";
+    		echo "<p>I ain't gon' smile if I don't want to</p>";
+    		echo "<p></p>";
+    		echo "<p>Hey man, we all can't be like you</p>";
+    		echo "<p></p>";
+    		echo "<p>I wish we were all rose-colored too</p>";
+    		echo "<p></p>";
+    		echo "<p>My rose-colored boy</p><br/><br/>";
+
+    		echo "<p>[BRIDGE]</p>";
+    		echo "<p></p>";
+    		echo "<p>Leave me here a little bit longer</p>";
+    		echo "<p></p>";
+    		echo "<p>I think I wanna stay in the car</p>";
+    		echo "<p>I don't want anybody seeing me cry now</p>";
+    		echo "<p></p>";
+    		echo "<p>You say 'We gotta look on the bright side'</p>";
+    		echo "<p></p>";
+    		echo "<p>I say 'Well maybe if you wanna go blind'</p>";
+    		echo "<p>You say my eyes are getting too dark now</p>";
+    		echo "<p>But boy, you ain't ever seen my mind</p><br/><br/>";
+
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p></p>";
+    		echo "<p>Just let me cry a little bit longer</p>";
+    		echo "<p></p>";
+    		echo "<p>I ain't gon' smile if I don't want to</p>";
+    		echo "<p></p>";
+    		echo "<p>Hey man, we all can't be like you</p>";
+    		echo "<p></p>";
+    		echo "<p>I wish we were all rose-colored too</p>";
+    		echo "<p></p>";
+    		echo "<p>My rose-colored boy</p>";
+    		echo "<p></p>";
+    		echo "<p>Just let me cry a little bit longer</p>";
+    		echo "<p></p>";
+    		echo "<p>I ain't gon' smile if I don't want to</p>";
+    		echo "<p></p>";
+    		echo "<p>Nah, we all can't be like you</p>";
+    		echo "<p></p>";
+    		echo "<p>I wish we were all rose-colored too</p>";
+    		echo "<p></p>";
+    		echo "<p>I wish we were all rose-colored too</p>";
+    		echo "<p></p>";
+    		echo "<p>My rose-colored boy</p>";
+    		echo "<p>Dm &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp  &nbsp C &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp F</p>";
+    		echo "<p>Low-key, no pressure, just hang with me and my weather</p>";
+    		echo "<p>Dm &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp  &nbsp C &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp F</p>";
+    		echo "<p>Low-key, no pressure, just hang with me and my weather</p>";
+    		
+
+
+
+ 
+
+    	  ?>
+ </div>
+	<footer>
+		<div class="footer">
+			Copyright &copy; 2022 Marella Buhay
+		</div>
+	</footer>
+
+</body>
+</html>
