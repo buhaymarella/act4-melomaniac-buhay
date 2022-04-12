80f5c58797b6d38e70c09450534a39962e5d9a95
buhaymarella <marella.gutierrez84@gmail.com>
2022-04-13 00:24:18 +0800

    SONG 1 ADDED

---
+++ song1.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>MELOMANIAC</title>
+</head>
+<body>
+	<nav>
+		<div class="brand_name">
+			<p>ROCK TRACK CORNER</p>
+		</div>
+		<ul>
+			<li><a href="song1.php" class="active">SONG 1</a></li>
+			<li><a href="song2.php">SONG 2</a></li>
+			<li><a href="song3.php">SONG 3</a></li>
+			<li><a href="song4.php">SONG 4</a></li>
+			<li><a href="song5.php">SONG 5</a></li>
+		</ul>
+	</nav>
+	<div class="image">
+		<div class="container">
+			<h1><span>WELCOME TO MELOMANIAC</span></h1>
+			<span class="caption"> Rock Out! Play Out! </span>
+		</div>		
+	</div>
+	<a href="https://www.youtube.com/watch?v=LW7FxWXt3ok"><img src="album/mcr.jfif" class="album"></a>
+    <h6 class="click"> Click on the photo to listen and play along! </h6>
+ </body>
+    <div class="lyrics">
+
+    	<?php
+    		echo "<br/>";
+    		echo "<br/>";
+    		echo "<h2 class='text-primary'>SUMMERTIME</h2>";
+    		echo "<p class='lead'> My Chemical Romance </p>";
+    		echo "<br/>";
+
+    		echo "<p>[INTRO: D  &nbsp A &nbsp Em &nbsp G]</p><br/><br/>";
+
+    		echo "<p>[VERSE 1]</p>";
+    		echo "<p>G &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D</p>";
+    		echo "<p>When the lights go out</p>";
+    		echo "<p> Will you take me with you?</p>";
+    		echo "A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Em";
+    		echo "<p>And carry all this broken bone</p>";
+    		echo "<p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspG</p>";
+    		echo "<p>Through six years down in crowded rooms</p>";
+    		echo "<p> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp D</p>";
+    		echo "<p>And highways I call home</p>";
+    		echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A</p>";
+    		echo "<p>Is something I can't know till now</p>";
+    		echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbspEm</p>";
+    		echo "<p> Till you picked me on the ground</p>";
+    		echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbspG</p>";
+    		echo "<p>With brick in hand, your lip gloss smile</p>";
+    		echo "<p>A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p> Your scrapped-up knees and </p><br/><br/>";
+
+    		echo "<p>[Chorus]</p>";
+    		echo "<p>G &nbsp &nbsp &nbsp &nbsp &nbsp A</p>";
+    		echo "<p>If you stay</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G</p>";
+    		echo "<p>I would even wait all night</p>";
+    		echo "<p>G&nbsp&nbsp &nbsp &nbsp &nbspA &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Bm</p>";
+    		echo "<p>Or until my heart explodes</p>";
+    		echo "<p>(Bm)&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>";
+    		echo "<p>How long? until we</p>";
+    		echo "<p>G &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>Find our way in the dark and out of harm</p>";
+    		echo "<p>G &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>You can run away with me... &nbsp anytime you want</p><br/><br/>";
+
+    		echo "<p>[VERSE 2]</p>";
+    		echo "<p>D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A</p>";
+    		echo "<p>Terrified of what i'd be</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Em</p>";
+    		echo "<p>As a kid from what I've seen</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G</p>";
+    		echo "<p>Every single day when people try</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D</p>";
+    		echo "<p>And put the pieces back together</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A</p>";
+    		echo "<p>Just to smash them down</p>";
+    		echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Em</p>";
+    		echo "<p>Turn my headphones up real loud</p>";
+    		echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp &nbsp G</p>";
+    		echo "<p>I dont't think I need them now</p>";
+    		echo "<p> &nbsp &nbsp A</p>";
+    		echo "<p>Cause you stop the noise</p><br/><br/>";
+
+    		echo "<p>[Chorus]</p>";
+    		echo "<p>G &nbsp &nbsp &nbsp &nbsp &nbsp A</p>";
+    		echo "<p>If you stay</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G</p>";
+    		echo "<p>I would even wait all night</p>";
+    		echo "<p>G &nbsp &nbsp &nbsp &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Bm</p>";
+    		echo "<p>Or until my heart explodes</p>";
+    		echo "<p>(Bm)&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>";
+    		echo "<p>How long? until we</p>";
+    		echo "<p>G &nbsp &nbsp &nbsp &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G</p>";
+    		echo "<p>Find our way in the dark and out of harm</p>";
+    		echo "<p>G &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>You can run away with me... &nbsp anytime you want</p>";
+    		echo "<p>D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>Well anytime you want</p>";
+    		echo "<p>D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>Well anytime you want</p><br/><br/>";
+
+    		echo "<p>[GUITAR SOLO]</p>";
+    		echo "<p>G &nbsp A &nbsp D &nbsp G &nbsp A &nbsp Bm &nbsp G &nbsp A &nbsp D &nbsp G &nbsp A &nbsp D</p><br/><br/>";
+
+    		echo "<p>[LAST CHORUS]</p>";
+    		echo "<p>D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>Don't walk away, dont't walk away</p>";
+    		echo "<p>D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G</p>";
+    		echo "<p>Don't walk away</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A</p>";
+    		echo "<p>If you stay</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp  D &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  G</p>";
+    		echo "<p>I would even wait all night</p>";
+    		echo "<p>G &nbsp &nbsp &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp Bm</p>";
+    		echo "<p>Or until my heart explodes</p>";
+    		echo "<p>(Bm)&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>How long? until we</p>";
+    		echo "<p>G &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp  &nbsp  &nbsp &nbsp  &nbsp &nbsp G</p>";
+    		echo "<p>Find our way in the dark and out of harm</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp G &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp A</p>";
+    		echo "<p>You can run away with me</p>";
+    		echo "<p>&nbsp Bm &nbsp &nbsp &nbsp </p>";
+    		echo "<p>You can write it i your arm</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp G &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  A</p>";
+    		echo "<p>You can run away with me...</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D</p>";
+    		echo "<p>Anytime you want</p><br/>";
+
+
+    		echo "<p>[ENDING]</p>";
+    		echo "<p>D</p>";
+
+    	 ?>
+ </div>
+	<footer>
+		<div class="footer">
+			Copyright &copy; 2022 Marella Buhay
+		</div>
+	</footer>
+
+</body>
+</html>
