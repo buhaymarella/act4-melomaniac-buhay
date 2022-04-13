5d88073f1b1bc69f314f3c16b686d32eed8eab1f
buhaymarella <marella.gutierrez84@gmail.com>
2022-04-13 17:19:31 +0800

    SONG 5 ADDED

---
+++ song5.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>MELOMANIAC</title>
+</head>
+<body>
+	<nav>
+		<div class="brand_name">
+			<p>ROCK TRACK CORNER</p>
+		</div>
+		<ul>
+			<li><a href="song1.php">SONG 1</a></li>
+			<li><a href="song2.php">SONG 2</a></li>
+			<li><a href="song3.php">SONG 3</a></li>
+			<li><a href="song4.php">SONG 4</a></li>
+			<li><a href="song5.php" class="active">SONG 5</a></li>
+		</ul>
+	</nav>
+	<div class="image">
+		<div class="container">
+			<h1><span>WELCOME TO MELOMANIAC</span></h1>
+			<span class="caption"> Rock Out! Play Out! </span>
+		</div>		
+	</div>
+	<a href="https://www.youtube.com/watch?v=9sTQ0QdkN3Q"><img src="album/bfmv.jfif" class="album"></a>
+    <h6 class="click"> Click on the photo to listen and play along! </h6>
+ </body>
+    <div class="lyrics">
+
+    	<?php
+    		echo "<br/>";
+    		echo "<br/>";
+    		echo "<h2 class='text-primary'>TEARS DONT'T FALL</h2>";
+    		echo "<p class='lead'> Bullet for my Valentine </p>";
+    		echo "<br/>";
+
+    		echo "<p>[INTRO]</p>";
+    		echo "<p>Em &nbsp  C &nbsp D (x2)</p>";
+    		echo "<p> C &nbsp D &nbsp C &nbsp D &nbsp Em  &nbsp D (x2) </p><br/><br/>";
+    		
+    		echo "<p>[VERSE 1]</p>";
+    		echo "<p>Em  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp C  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp D &nbsp   &nbsp  &nbsp </p>";
+    		echo "<p>With bloodshot eyes I watch you sleeping</p>";
+    		echo "<p>Em  &nbsp  &nbsp  &nbsp &nbsp &nbsp   &nbsp  &nbsp  &nbsp &nbsp &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp C  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp &nbsp &nbspD &nbsp   &nbsp  &nbsp &nbsp &nbsp </p>";
+    		echo "<p>The warmth I feel beside me is slowly fading</p><br/><br/>";
+
+    		echo "<p>[BRIDGE]</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Em &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp C   &nbsp  &nbsp  &nbsp &nbsp  &nbsp D</p>";
+    		echo "<p>Would she hear me if I called her name?</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Em &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp C   &nbsp  &nbsp  &nbsp &nbsp  &nbsp D</p>";
+    		echo "<p>Would she hold me if she knew my shame?</p><br/><br/>";
+    		
+    		
+    		echo "<p>[PRE CHORUS]</p>";
+    		echo "<p>Em &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>There's always something different going wrong</p>";
+    		echo "<p>C &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>The path I walk's in the wrong direction</p>";
+    		echo "<p>Em &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>There's always someone fucking hanging on</p>";
+    		echo "<p>C &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>Can anybody help me make it better?</p><br/><br/>";
+    		
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p>C &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp C</p>";
+    		echo "<p>Your tears don't fall, they crash around me</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp D &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Em   &nbsp  &nbsp  &nbsp &nbsp  &nbsp D  &nbsp C</p>";
+    		echo "<p>Her conscience calls, the guilty to come home</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  C </p>";
+    		echo "<p>Your tears don't fall, they crash around me</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp&nbsp &nbsp Em &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp Em</p>";
+    		echo "<p>Her conscience calls, the guilty to come home</p><br/><br/>";
+
+    		echo "<p>[VERSE 2]</p>";
+    		echo "<p>Em  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp C  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp D &nbsp   &nbsp  &nbsp </p>";
+    		echo "<p>The moments die, I hear no screaming</p>";
+    		echo "<p>Em  &nbsp  &nbsp  &nbsp &nbsp &nbsp   &nbsp  &nbsp  &nbsp &nbsp &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp C  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp &nbsp &nbspD &nbsp   &nbsp  &nbsp &nbsp &nbsp </p>";
+    		echo "<p>The visions left inside me are slowly fading</p><br/><br/>";
+
+    		echo "<p>[BRIDGE]</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Em &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp C   &nbsp  &nbsp  &nbsp &nbsp  &nbsp D</p>";
+    		echo "<p>Would she hear me if I called her name?</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Em &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp C   &nbsp  &nbsp  &nbsp &nbsp  &nbsp D</p>";
+    		echo "<p>Would she hold me if she knew my shame?</p><br/><br/>";
+    		
+    		
+    		echo "<p>[PRE CHORUS]</p>";
+    		echo "<p>Em &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>There's always something different going wrong</p>";
+    		echo "<p>C &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>The path I walk's in the wrong direction</p>";
+    		echo "<p>Em &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>There's always someone fucking hanging on</p>";
+    		echo "<p>C &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>Can anybody help me make it better?</p><br/><br/>";
+    		
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p>C &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp C</p>";
+    		echo "<p>Your tears don't fall, they crash around me</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp D &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Em   &nbsp  &nbsp  &nbsp &nbsp  &nbsp D  &nbsp C</p>";
+    		echo "<p>Her conscience calls, the guilty to come home</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  C </p>";
+    		echo "<p>Your tears don't fall, they crash around me</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp&nbsp &nbsp Em &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp Em</p>";
+    		echo "<p>Her conscience calls, the guilty to come home</p><br/><br/>";
+
+
+    		echo "<p>[SOLO]</p><br/><br/>";
+
+    		echo "<p>[OUTRO]</p>";
+    		echo "<p>C &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>Your tears don't fall, they crash around me</p>";
+    		echo "<p>C &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp Em &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D</p>";
+    		echo "<p>Her conscience calls, the guilty to come home</p><br/><br/>";
+
+
+
+
+ 
+
+    	  ?>
+ </div>
+	<footer>
+		<div class="footer">
+			Copyright &copy; 2022 Marella Buhay
+		</div>
+	</footer>
+
+</body>
+</html>
