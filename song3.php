4f8f6e1fed2bec18c0dc4bad7c300856357138e3
buhaymarella <marella.gutierrez84@gmail.com>
2022-04-13 15:15:24 +0800

    SONG 3 ADDED

---
+++ song3.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>MELOMANIAC</title>
+</head>
+<body>
+	<nav>
+		<div class="brand_name">
+			<p>ROCK TRACK CORNER</p>
+		</div>
+		<ul>
+			<li><a href="song1.php">SONG 1</a></li>
+			<li><a href="song2.php">SONG 2</a></li>
+			<li><a href="song3.php" class="active">SONG 3</a></li>
+			<li><a href="song4.php">SONG 4</a></li>
+			<li><a href="song5.php">SONG 5</a></li>
+		</ul>
+	</nav>
+	<div class="image">
+		<div class="container">
+			<h1><span>WELCOME TO MELOMANIAC</span></h1>
+			<span class="caption"> Rock Out! Play Out! </span>
+		</div>		
+	</div>
+	<a href="https://www.youtube.com/watch?v=djV11Xbc914"><img src="album/aha.jfif" class="album"></a>
+    <h6 class="click"> Click on the photo to listen and play along! </h6>
+ </body>
+    <div class="lyrics">
+
+    	<?php
+    		echo "<br/>";
+    		echo "<br/>";
+    		echo "<h2 class='text-primary'>TAKE ON ME</h2>";
+    		echo "<p class='lead'> A-HA </p>";
+    		echo "<br/>";
+
+    		echo "<p>[INTRO]</p>";
+    		echo "<p>Bm &nbsp  E &nbsp A &nbsp D &nbsp A/C#</p>";
+    		echo "<p> Bm &nbsp E &nbsp D/F# &nbsp Esus2 &nbsp E</p><br/><br/>";
+    		
+    		echo "<p>[VERSE 1]</p>";
+    		echo "<p>Bm &nbsp &nbsp &nbsp E &nbsp &nbsp &nbsp &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp &nbsp&nbsp A/C#</p>";
+    		echo "<p>Talking away,  I don’t know what I’m to say</p>";
+    		echo "<p>Bm &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp E &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp A/C# &nbsp &nbsp</p>";
+    		echo "<p>I’ll say it anyway,Today’s another day to find you</p>";
+    		echo "<p>Bm &nbsp &nbsp E &nbsp F#m &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp </p>";
+    		echo "<p>Shyin' away, I’ll be coming for your love, OK?</p><br/><br/>";
+
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p>A &nbsp &nbsp E/G# &nbsp F#m D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>Take on me  (take on me)</p>";
+    		echo "<p>A &nbsp &nbsp E/G# &nbsp F#m D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>Take on me  (take on me)</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp A &nbsp E/G# &nbsp F#m D</p>";
+    		echo "<p>I’ll be gone</p>";
+    		echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A &nbsp E &nbsp D/F# E</p>";
+    		echo "<p>in a day or two</p><br/><br/>";
+    		
+    		echo "<p>[VERSE 2]</p>";
+    		echo "<p>Bm &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp E &nbsp &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp &nbsp&nbsp A/C#</p>";
+    		echo "<p>So, needless to say I'm odds and ends, but I'll be</p>";
+    		echo "<p>Bm &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp E &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp A/C# &nbsp &nbsp</p>";
+    		echo "<p>stumbling awake slowly learning that life is OK,</p>";
+    		echo "<p>Bm &nbsp &nbsp E &nbsp F#m &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp </p>";
+    		echo "<p>Say after me, it’s no better to be safe than sorry</p><br/><br/>";
+    		
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p>A &nbsp &nbsp E/G# &nbsp F#m D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>Take on me  (take on me)</p>";
+    		echo "<p>A &nbsp &nbsp E/G# &nbsp F#m D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>Take on me  (take on me)</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp A &nbsp E/G# &nbsp F#m D</p>";
+    		echo "<p>I’ll be gone</p>";
+    		echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A &nbsp E &nbsp D/F# E</p>";
+    		echo "<p>in a day or two</p><br/><br/>";
+
+    		echo "<p>[INSTRUMENTAL]</p>";
+    		echo "<p>C#m &nbsp G</p>";
+    		echo "<p>C#m &nbsp G</p>";
+    		echo "<p>Bm &nbsp E</p><br/><br/>";
+
+    		echo "<p>[VERSE 3]</p>";
+    		echo "<p>Bm &nbsp &nbsp &nbsp  &nbsp  &nbsp &nbsp &nbsp &nbsp E &nbsp &nbsp A &nbsp  &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp &nbsp&nbsp A/C#</p>";
+    		echo "<p>Oh, things that you say, yeah is it life or just to play</p>";
+    		echo "<p>Bm &nbsp &nbsp &nbsp &nbsp  &nbsp  &nbsp  &nbsp &nbsp &nbsp E &nbsp  &nbsp  &nbsp A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp A/C# &nbsp &nbsp</p>";
+    		echo "<p>My worries away, you’re all the things I’ve got to remember</p>";
+    		echo "<p>Bm &nbsp &nbsp &nbsp  &nbsp  &nbsp  E &nbsp F#m &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D &nbsp &nbsp </p>";
+    		echo "<p>You are shying away, I’ll be coming for you anyway</p><br/><br/>";
+
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p>A &nbsp &nbsp E/G# &nbsp F#m D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>Take on me  (take on me)</p>";
+    		echo "<p>A &nbsp &nbsp E/G# &nbsp F#m D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>Take on me  (take on me)</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp A &nbsp E/G# &nbsp F#m D</p>";
+    		echo "<p>I’ll be gone</p>";
+    		echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A &nbsp E &nbsp D/F# E</p>";
+    		echo "<p>in a day or two</p><br/>";
+    		echo "<p>A &nbsp &nbsp E/G# &nbsp F#m D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>Take on me  (take on me)</p>";
+    		echo "<p>A &nbsp &nbsp E/G# &nbsp F#m D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp</p>";
+    		echo "<p>Take on me  (take on me)</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp A &nbsp E/G# &nbsp F#m D</p>";
+    		echo "<p>I’ll be gone</p>";
+    		echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A &nbsp E &nbsp D/F# E</p>";
+    		echo "<p>in a day or two</p><br/><br/>";
+
+
+
+ 
+
+    	  ?>
+ </div>
+	<footer>
+		<div class="footer">
+			Copyright &copy; 2022 Marella Buhay
+		</div>
+	</footer>
+
+</body>
+</html>
