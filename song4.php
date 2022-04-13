c30cdcfda0556bab7035b31124bca2b1f3737d15
buhaymarella <marella.gutierrez84@gmail.com>
2022-04-13 16:27:30 +0800

    SONG 4 ADDED

---
+++ song4.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>MELOMANIAC</title>
+</head>
+<body>
+	<nav>
+		<div class="brand_name">
+			<p>ROCK TRACK CORNER</p>
+		</div>
+		<ul>
+			<li><a href="song1.php">SONG 1</a></li>
+			<li><a href="song2.php">SONG 2</a></li>
+			<li><a href="song3.php">SONG 3</a></li>
+			<li><a href="song4.php" class="active">SONG 4</a></li>
+			<li><a href="song5.php">SONG 5</a></li>
+		</ul>
+	</nav>
+	<div class="image">
+		<div class="container">
+			<h1><span>WELCOME TO MELOMANIAC</span></h1>
+			<span class="caption"> Rock Out! Play Out! </span>
+		</div>		
+	</div>
+	<a href="https://www.youtube.com/watch?v=LXEKuttVRIo"><img src="album/slipknot.jfif" class="album"></a>
+    <h6 class="click"> Click on the photo to listen and play along! </h6>
+ </body>
+    <div class="lyrics">
+
+    	<?php
+    		echo "<br/>";
+    		echo "<br/>";
+    		echo "<h2 class='text-primary'>SNUFF</h2>";
+    		echo "<p class='lead'> Slipknot </p>";
+    		echo "<br/>";
+
+    		echo "<p>[INTRO]</p>";
+    		echo "<p>Am &nbsp Em &nbsp F&nbsp C</p><br/><br/>";
+    		
+    		echo "<p>[VERSE 1]</p>";
+    		echo "<p>Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Em  &nbsp F &nbsp C</p>";
+    		echo "<p>Bury all your secrets in my skin</p>";
+    		echo "<p>Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Em  &nbsp F &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp C</p>";
+    		echo "<p>Come away with innocence, and leave me with my sins</p>";
+    		echo "<p>Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Em &nbsp &nbsp &nbsp &nbsp  &nbsp F &nbsp C</p>";
+    		echo "<p>The air around me still feels like a cage</p>";
+    		echo "<p>Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Em  &nbsp &nbsp F &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp  C</p>";
+    		echo "<p>And love is just a camouflage for what resembles rage again…</p><br/><br/>";
+
+    		echo "<p>[PRE-CHORUS]</p>";
+    		echo "<p>Am &nbsp Em &nbsp F&nbsp C</p><br/><br/>";
+
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Am</p>";
+    		echo "<p>So if you love me, let me go. And run away before I know.</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Am</p>";
+    		echo "<p>My heart is just too dark to care. I can't destroy what isn't there.</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Am</p>";
+    		echo "<p>Deliver me into my Fate - If I'm alone I cannot hate</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G</p>";
+    		echo "<p>I don't deserve to have you…</p>";
+    		echo "<p>F &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Am</p>";
+    		echo "<p>My smile was taken long ago / If I can change I hope I never know</p><br/><br/>";
+
+    		echo "<p>[PRE-VERSE]</p>";
+    		echo "<p>Am &nbsp Em &nbsp F&nbsp C</p><br/><br/>";
+    		
+    		echo "<p>[VERSE 2]</p>";
+    		echo "<p>Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Em  &nbsp F &nbsp C</p>";
+    		echo "<p>I still press your letters to my lips</p>";
+    		echo "<p>Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp Em  &nbsp &nbsp &nbsp  F &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   C</p>";
+    		echo "<p>And cherish them in parts of me that savor every kiss</p>";
+    		echo "<p>Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Em  &nbsp F &nbsp C</p>";
+    		echo "<p>I couldn't face a life without your light</p>";
+    		echo "<p>Am &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp Em&nbsp &nbsp  F &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp C</p>";
+    		echo "<p>But all of that was ripped apart… when you refused to fight</p><br/><br/>";
+    		
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbspG &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Am</p>";
+    		echo "<p>So save your breath, I will not hear. I think I made it very clear.</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbspG &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Am</p>";
+    		echo "<p>You couldn't hate enough to love. Is that supposed to be enough?</p>";
+    		echo "<p>  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbspG  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Am</p>";
+    		echo "<p>I only wish you weren't my friend. Then I could hurt you in the end.</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp   &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp   &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp G</p>";
+    		echo "<p>I never claimed to be a Saint…</p>";
+    		echo "<p>&nbsp F &nbsp  &nbsp  &nbsp  &nbsp  &nbsp   &nbsp  &nbsp  &nbsp  &nbsp  &nbsp   &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Am</p>";
+    		echo "<p>My own was banished long ago / It took the Death of Hope to let you go</p><br/><br/>";
+
+    		echo "<p>[SOLO]</p>";
+    		echo "<p>Am &nbsp Em &nbsp F&nbsp C</p><br/><br/>";
+
+    		echo "<p>[CHORUS]</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp G</p>";
+    		echo "<p>So Break Yourself Against My Stones</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Am</p>";
+    		echo "<p>And Spit Your Pity In My Soul</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp G</p>";
+    		echo "<p>You Never Needed Any Help</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Am</p>";
+    		echo "<p>You Sold Me Out To Save Yourself</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbspG</p>";
+    		echo "<p>And I Won't Listen To Your Shame</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp Am</p>";
+    		echo "<p>You Ran Away - You're All The Same</p>";
+    		echo "<p> &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbsp  &nbspG</p>";
+    		echo "<p>Angels Lie To Keep Control…</p><br/><br/>";
+
+    		echo "<p>[OUTRO]</p>";
+    		echo "<p>&nbsp F &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp </p>";
+    		echo "<p>My Love Was Punished Long Ago</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A5 B5 C5 E5 F5 </p>";
+    		echo "<p>If You Still Care, Don't Ever Let Me Know</p>";
+    		echo "<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  A5 B5 C5 E5 F5 (2X) Am</p>";
+    		echo "<p>If you still care, don't ever let me know…</p><br/><br/>";
+
+
+ 
+
+    	  ?>
+ </div>
+	<footer>
+		<div class="footer">
+			Copyright &copy; 2022 Marella Buhay
+		</div>
+	</footer>
+
+</body>
+</html>
